e907a1b4117792decffcaae66abd68d91a2670a5
looklikeapro <jerryzhou00@gmail.com>
2016-01-21 21:13:59 -0500

    finish bc scrape

--- lib/scrapers/bc_farmers_market.rb
+++ lib/scrapers/bc_farmers_market.rb
@@ -10,8 +10,8 @@ module Scrapers
 			puts "Done collecting links"
 			farms = []
 			links.each do |link|
+				puts "Scraping "+ link
 				farms.push(scrape_farm(link))
-				puts "Completed "+ link
 			end
 			return farms
 		end
@@ -31,43 +31,28 @@ module Scrapers
 			doc = Nokogiri::HTML(open(BCFarmersMarket::SITE+link))
 			farm = {}
 			farm["name"] = doc.css(".vendor-title h1")[0].content.strip
-			address_dump = doc.css('#venodr-location .location-description')[0].content.strip
-			farm["hours"] = doc.css(".market-timings")[0].content.strip.sub("Hours of operation:", "")
+			begin
+				farm["hours"] = doc.css(".market-timings")[0].content.strip.sub("Hours of operation:", "")
+			rescue Exception => e
+			end
 			farm["description"] = doc.css(".vendor-description")[0].content.strip
 			farm["phone"] = doc.css(".vendor-phone")[0].content.strip.sub("Phone:", '')
 			farm["email"] = doc.css(".vendor-email")[0].content.strip.sub("Email:", '')
+			begin
+				farm["website"] = doc.css(".vendor-website a")[0].content.strip
+			rescue Exception => e
+			end
 			products_string = ""
 			doc.css(".vendor-products ul")[0].children.each do |item|
 				products_string += item.content.strip + ", "
 			end
 			farm["products"] = products_string
-			address_dict = Geolocation::Geolocator.get_geo(address_dump + ", BC, Canada")
-			farm = farm.merge(address_dict)
-
-			# farm['street'] = doc_body.css('.elocator_address h2')[0].content.strip
-			# begin
-			# 	farm['city'] = doc_body.css('.elocator_address h2')[1].content.split(', ')[0].strip
-			# 	farm['province'] = doc_body.css('.elocator_address h2')[1].content.split(',')[1].strip
-			# 	farm['country'] = doc_body.css('.elocator_address h2')[1].content.split(',')[2].strip
-			# rescue Exception => e
-			# 	farm['address_dump'] = doc_body.css('.elocator_address h2')[1].content.strip
-			# end
-			# farm['postal_code'] = doc_body.css('.elocator_address h2')[2].content.strip
-			# begin
-			# 	farm['phone'] = doc_body.css('.elocator_address p')[0].content.sub!('Phone:', '').strip
-			# 	farm['hours'] = doc_body.css('.elocator_address p')[1].content.sub!('Hours:', '').strip
-			# rescue Exception => e
-			# end
-			# begin
-			# 	farm['link'] = doc_body.css('.elocator_address h2 a')[0]['href']
-			# rescue Exception => e
-			# end
-			# begin
-			# 	farm['products'] = doc_body.css('.elocator_public_div .el_product_div span')[0].content.strip
-			# 	farm['products_grown'] = doc_body.css('.elocator_public_div .el_product_div span')[1].content.strip
-			# 	farm['products_pick'] = doc_body.css('.elocator_public_div .el_product_div span')[2].content.strip
-			# rescue Exception => e
-			# end
+			begin
+				address_dump = doc.css('#venodr-location p')[0].content.strip
+				address_dict = Geolocation::Geolocator.get_geo(address_dump + ", Canada")
+				farm = farm.merge(address_dict)
+			rescue Exception => e
+			end
 			return farm
 		end
 		private_class_method :collect_links, :scrape_farm
